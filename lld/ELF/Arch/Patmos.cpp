cf584182a35941d45f6adb1fa63e8694b46d1f14
haraldschn <20490205+haraldschn@users.noreply.github.com>
2021-12-21 13:47:58 +0100

    [Patmos][lld] Added Relocations + Deleted Methods for PLT and GOT

--- lld/ELF/Arch/Patmos.cpp
+++ lld/ELF/Arch/Patmos.cpp
@@ -24,76 +24,26 @@ namespace {
 class Patmos final : public TargetInfo {
 public:
   Patmos();
-  uint32_t calcEFlags() const override;
-  void writeGotHeader(uint8_t *buf) const override;
-  void writeGotPlt(uint8_t *buf, const Symbol &s) const override;
-  void writePltHeader(uint8_t *buf) const override;
-  void writePlt(uint8_t *buf, const Symbol &sym,
-                uint64_t pltEntryAddr) const override;
-  RelType getDynRel(RelType type) const override;
   RelExpr getRelExpr(RelType type, const Symbol &s,
                      const uint8_t *loc) const override;
   void relocate(uint8_t *loc, const Relocation &rel,
                 uint64_t val) const override;
 };
 
-} // end anonymous namespace
+}
 
-// How to hande
-// difference 32-bit and 64-bit bundle format
 
 
 Patmos::Patmos() {
-  noneRel = R_PATMOS_NONE;
-  
-  defaultCommonPageSize = 0x1000;
+  //defaultImageBase = 0x00020000;
+  defaultImageBase = 0x00001EA00; 
   defaultMaxPageSize = 0x1000;
-}
+  defaultCommonPageSize = 0x1000;
 
-static uint32_t getEFlags(InputFile *f) {
-  // TODO: Find out, why this Method exists in other Targets ?
-  return cast<ObjFile<ELF32BE>>(f)->getObj().getHeader().e_flags;
-}
+  needsThunks = true;
 
-uint32_t Patmos::calcEFlags() const {
-  // FLAGS for other Targest are defined in llvm/BinaryFormat/ELF.h
-  // Where is this Method calcEFlags() needed in lld 
+  noneRel = R_PATMOS_NONE;
 
-  // If there are only binary input files (from -b binary), use a
-  // value of 0 for the ELF header flags.
-  if (objectFiles.empty())
-    return 0;
-
-  uint32_t target = getEFlags(objectFiles.front());
-
-  return target;
-}
-
-void Patmos::writeGotHeader(uint8_t *buf) const {
-  // TODO: Find out, why this Method exists in other Targets ?
-  // GOT - Global Operations Table
-
-  write32be(buf, mainPart->dynamic->getVA());  
-}
-
-void Patmos::writeGotPlt(uint8_t *buf, const Symbol &s) const {
-  // TODO: Find out, why this Method exists in other Targets ?
-  // PLT - Procedure Logik Table
-
-  write32be(buf, mainPart->dynamic->getVA());    
-}
-
-void Patmos::writePltHeader(uint8_t *buf) const {
-  // TODO: Find out, why this Method exists in other Targets ?
-}
-
-void Patmos::writePlt(uint8_t *buf, const Symbol &sym,
-                     uint64_t pltEntryAddr) const {
-  // TODO: Find out, why this Method exists in other Targets ?
-}
-
-RelType Patmos::getDynRel(RelType type) const {
-  return type == target->symbolicRel ? type : static_cast<RelType>(R_PATMOS_NONE);
 }
 
 RelExpr Patmos::getRelExpr(const RelType type, const Symbol &s,
@@ -134,7 +84,6 @@ static uint32_t extractBits(uint64_t v, uint32_t begin, uint32_t end) {
 void Patmos::relocate(uint8_t *loc, const Relocation &rel, uint64_t val) const {
   // Relocate Types defined in /llvm/include/llvm/BinaryFormat/ELF.h
   
-
   switch (rel.type) {
   case R_PATMOS_CFLI_ABS: {
     checkInt(loc, static_cast<int64_t>(val) >> 2, 22, rel);
@@ -145,14 +94,60 @@ void Patmos::relocate(uint8_t *loc, const Relocation &rel, uint64_t val) const {
     write32be(loc,insn);
     return;
   }
-  case R_PATMOS_ALUI_ABS:
-    //error("Loc:" + getErrorLocation(loc) + " val: " + toString(val));
-  case R_PATMOS_ALUL_ABS:
-  case R_PATMOS_MEMB_ABS:
-  case R_PATMOS_MEMH_ABS:
-  case R_PATMOS_MEMW_ABS:
-  case R_PATMOS_ABS_32:
+  case R_PATMOS_ALUI_ABS: {
+    checkInt(loc, static_cast<int64_t>(val), 12, rel);
+
+    uint32_t insn = read32be(loc);
+    uint32_t imm =  extractBits(val, 11, 0);
+    insn |= imm;
+    write32be(loc,insn);
     return;
+  }
+  case R_PATMOS_ALUL_ABS: {
+    checkInt(loc, static_cast<int64_t>(val) >> 2, 32, rel);
+
+    uint64_t insn = read64be(loc);
+    uint32_t imm =  extractBits(val, 31, 0);
+    insn |= imm;
+    write64be(loc,insn);
+    return;
+  }
+  case R_PATMOS_MEMB_ABS:{
+    checkInt(loc, static_cast<int64_t>(val), 7, rel);
+
+    uint64_t insn = read32be(loc);
+    uint32_t imm =  extractBits(val, 6, 0);
+    insn |= imm;
+    write32be(loc,insn);
+    return;
+  }
+  case R_PATMOS_MEMH_ABS:{
+    checkInt(loc, static_cast<int64_t>(val) >> 1, 7, rel);
+
+    uint64_t insn = read32be(loc);
+    uint32_t imm =  extractBits(val, 7, 1);
+    insn |= imm;
+    write32be(loc,insn);
+    return;
+  }
+  case R_PATMOS_MEMW_ABS:{
+    checkInt(loc, static_cast<int64_t>(val) >> 2, 7, rel);
+
+    uint64_t insn = read32be(loc);
+    uint32_t imm =  extractBits(val, 8, 2);
+    insn |= imm;
+    write32be(loc,insn);
+    return;
+  }
+  case R_PATMOS_ABS_32: {
+    
+    checkInt(loc, static_cast<int64_t>(val), 32, rel);
+
+    uint32_t temp =  extractBits(val, 31, 0);
+    write32be(loc,temp);
+    
+    return;
+  }
   case R_PATMOS_CFLI_PCREL: {
     checkInt(loc, static_cast<int64_t>(val) >> 2, 22, rel);
 
