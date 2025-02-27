37b15a9730a8e41b13f037f412d32429cf1ff258
Emad Jacob Maroun <emoun.open@gmail.com>
2025-02-27 08:58:26 +0100

    Update compile-only-archives.c

--- clang/test/Driver/Patmos/compile-only-archives.c
+++ clang/test/Driver/Patmos/compile-only-archives.c
@@ -1,8 +1,8 @@
 // RUN: %clang --target=patmos %S/helpers/helper-function.c -c -o %t-object.o
 // RUN: %clang --target=patmos %S/helpers/helper-function2.c -c -o %t-object2.o
-// RUN: ar cr %t-archive.a %t-object.o %t-object2.o
+// RUN: ar crs %t-archive.a %t-object.o %t-object2.o
 // RUN: %clang --target=patmos %s -c -o %t-object3.o
-// RUN: ar cr %t-archive2.a %t-object3.o
+// RUN: ar crs %t-archive2.a %t-object3.o
 // RUN: %clang -v --target=patmos %t-archive.a %t-archive2.a -o %t
 // RUN: llvm-objdump -rd %t | FileCheck %s
 // END.
